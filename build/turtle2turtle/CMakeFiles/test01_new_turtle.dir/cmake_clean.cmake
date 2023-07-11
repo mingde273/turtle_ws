e4509e45ecaf5574dcec7a77493ac267b3c904df
yanglang <2731910192@qq.com>
2023-07-11 20:50:23 +0800

    02

---
+++ build/turtle2turtle/CMakeFiles/test01_new_turtle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/yl/turtle_ws/devel/lib/turtle2turtle/test01_new_turtle"
+  "/home/yl/turtle_ws/devel/lib/turtle2turtle/test01_new_turtle.pdb"
+  "CMakeFiles/test01_new_turtle.dir/src/test01_new_turtle.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test01_new_turtle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/turtle2turtle/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yl/turtle_ws/src/turtle2turtle
+# Build directory: /home/yl/turtle_ws/build/turtle2turtle
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
