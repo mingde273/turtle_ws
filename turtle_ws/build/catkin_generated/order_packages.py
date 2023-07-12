6fbd44b614d2ed55311c3f52fbbec43ba7118055
yanglang <2731910192@qq.com>
2023-07-12 09:50:04 +0800

    04

--- turtle_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/yl/turtle_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/yl/turtle_ws/devel;/opt/ros/noetic'.split(';') if '/home/yl/turtle_ws/devel;/opt/ros/noetic' != '' else []
--- turtle_ws/build/turtle2turtle/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yl/turtle_ws/src/turtle2turtle
-# Build directory: /home/yl/turtle_ws/build/turtle2turtle
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
